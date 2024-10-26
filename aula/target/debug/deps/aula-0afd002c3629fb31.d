f82d29df8f21a314b44ec84ddabd859837fcd4cd
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-10-26 13:30:54 -0300

    .

---
+++ aula/target/debug/deps/aula-0afd002c3629fb31.d
@@ -0,0 +1,5 @@
+C:\Users\Vitorio.amc\Documents\GitHub\rust\aula\target\debug\deps\libaula-0afd002c3629fb31.rmeta: src/main.rs
+
+C:\Users\Vitorio.amc\Documents\GitHub\rust\aula\target\debug\deps\aula-0afd002c3629fb31.d: src/main.rs
+
+src/main.rs:
---
+++ aula/target/debug/deps/aula-ac4c6322aab09788.d
@@ -0,0 +1,5 @@
+C:\Users\Vitorio.amc\Documents\GitHub\rust\aula\target\debug\deps\libaula-ac4c6322aab09788.rmeta: src/main.rs
+
+C:\Users\Vitorio.amc\Documents\GitHub\rust\aula\target\debug\deps\aula-ac4c6322aab09788.d: src/main.rs
+
+src/main.rs:
